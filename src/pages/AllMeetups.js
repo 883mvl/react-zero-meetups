4ba8fc32fbc5b516680baf50cc0165310c730f00
BS667 <tahsen@myvenuelive.com>
2021-10-11 16:43:57 +0600

    First commit

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import { url } from "../env";
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
